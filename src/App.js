9429eeb87f45af1e343293a90941cb010332e4eb
mdasif8393 <md.asif.8393@gmail.com>
2023-02-03 11:17:16 +0600

    refactore app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import store from "./redux/store";
 import routes from "./routes/routes";
 
 function App() {
   return (
     <div>
-      <Provider store={store}>
         <RouterProvider router={routes} />
-      </Provider>
     </div>
   );
 }
