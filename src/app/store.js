b1dd5af3084a8c6a23d3faca80b57fd5edb22923
mdasif8393 <md.asif.8393@gmail.com>
2023-02-03 21:31:32 +0600

    complete

---
+++ src/app/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "../features/cart/cartSlice";
+import filterSlice from "../features/filter/filterSlice";
+import logger from "redux-logger";
+import productSlice from "../features/products/productsSlice";
+
+const store = configureStore({
+    reducer: {
+        cart: cartSlice,
+        filter: filterSlice,
+        products: productSlice,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+})
+
+export default store;
--- src/pages/Main/Cart.js
+++ src/pages/Main/Cart.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ProductCard from "../../components/ProductCard";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
 
+  const cart = useSelector(state => state.cart.cart);
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-
+      {
+        cart.map(product => <ProductCard product={product}></ProductCard>)
+      }
     </div>
   );
 };
