08089a58d187b9baeb3a3e46bbeaa58fa423fde6
mdasif8393 <md.asif.8393@gmail.com>
2023-02-04 23:50:40 +0600

    add

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,23 +1,48 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { deleteProduct, fetchProducts, postProduct } from "./productsAPI";
 
 const initialState = {
     products: [],
     isLoading: false,
+    postSuccess: false,
+    deleteSuccess: false,
     isError: false,
     error: "",
 };
 
 export const getProducts = createAsyncThunk("products/getProduct", async () => {
-    const res = await fetch("http://localhost:5000/products");
-    const data = await res.json();
-    return data.data;
+    const products = fetchProducts()
+    return products;
+});
+
+export const addProduct = createAsyncThunk("products/addProduct", async (data) => {
+    const products = postProduct(data)
+    return products;
+});
+
+export const removeProduct = createAsyncThunk("products/removeProduct", async (id, thunkAPI) => {
+    const products = await deleteProduct(id);
+    thunkAPI.dispatch(removeFromList(id));
+    return products;
 });
 
 const productSlice = createSlice({
     name: "products",
     initialState,
+    reducers: {
+        togglePostSuccess: (state) => {
+            state.postSuccess = false;
+        },
+        toggleDeleteSuccess: (state) => {
+            state.deleteSuccess = false;
+        },
+        removeFromList: (state, action) => {
+            state.products = state.products.filter(product => product._id !== action.payload )
+        }
+    },
     extraReducers: (builder) => {
-        builder.addCase(getProducts.pending, (state, action)=>{
+        builder
+        .addCase(getProducts.pending, (state)=>{
             state.isLoading = true;
             state.isError = false;
 
@@ -31,9 +56,45 @@ const productSlice = createSlice({
             state.isLoading = false;
             state.isError = true;
             state.error = action.error.message;
+        })
+        .addCase(addProduct.pending, (state)=>{
+            state.isLoading = true;
+            state.postSuccess = false;
+            state.isError = false;
+
+        })
+        .addCase(addProduct.fulfilled, (state)=>{
+            state.postSuccess = true;
+            state.isLoading = false;
+        })
+        .addCase(addProduct.rejected, (state, action)=>{
+            state.products = [];
+            state.isLoading = false;
+            state.postSuccess = false;
+            state.isError = true;
+            state.error = action.error.message;
+        })
+        .addCase(removeProduct.pending, (state)=>{
+            state.isLoading = true;
+            state.deleteSuccess = false;
+            state.isError = false;
+
+        })
+        .addCase(removeProduct.fulfilled, (state)=>{
+            state.deleteSuccess = true;
+            state.isLoading = false;
+        })
+        .addCase(removeProduct.rejected, (state, action)=>{
+            state.products = [];
+            state.isLoading = false;
+            state.deleteSuccess = false;
+            state.isError = true;
+            state.error = action.error.message;
         });
         
     },
 })
+
+export const { togglePostSuccess, removeFromList, toggleDeleteSuccess } = productSlice.actions;
 
 export default productSlice.reducer;
